5a8736eb6022b4f82d419844ae71df774fbf04d0
chriskish19 <68289629+chriskish19@users.noreply.github.com>
2025-03-16 09:36:33 -0600

    working on logger

--- projects/main_project_jujubee/classes/logging/logs.cpp
+++ projects/main_project_jujubee/classes/logging/logs.cpp
@@ -1,30 +1,39 @@
-#include "logs.hpp"
 #include NAMES_INCLUDE
 #include LOGS_INCLUDE_PATH
+
+classes::log::log(std::size_t message_length)
+    :m_message_length_reserved(message_length)
+{
+    m_message.reserve(message_length);
+}
 
 void classes::log::set_message(const string& message)
 {
     m_message = message;
 }
 
-std::chrono::steady_clock::time_point classes::log::get_time()
-{
-    m_time = std::chrono::high_resolution_clock::now();
-    return m_time;
-}
-
 void classes::log::set_time()
 {
     m_time = std::chrono::high_resolution_clock::now();
+    m_stime = std::format(ROS("[{}]"), m_time);
 }
 
-classes::base_logger::base_logger(std::size_t log_count)
-    :m_log_count(log_count)
+classes::base_logger::base_logger(std::size_t log_count, std::size_t message_length)
+    :m_log_count(log_count),m_message_length(message_length)
 {
     m_logs_v.reserve(m_log_count);
 
     for (std::size_t i = 0; i < m_log_count; ++i) {
-        log* lp = new log;
+        log* lp = new log(m_message_length);
         m_logs_v.push_back(lp);
+    }
+}
+
+classes::base_logger::~base_logger()
+{
+    for (auto lp : m_logs_v) {
+        if (lp != nullptr) {
+            delete lp;
+        }
     }
 }
--- projects/main_project_jujubee/classes/logging/logs.hpp
+++ projects/main_project_jujubee/classes/logging/logs.hpp
@@ -15,23 +15,32 @@
 namespace classes {
 	class log {
 	public:
-		log() = default;
+		log(std::size_t message_length);
 
 		void set_message(const string& message);
 		string get_message() { return m_message; }
-		std::chrono::steady_clock::time_point get_time();
 		void set_time();
 	protected:
+		// the log message
 		string m_message;
+
+		// the time stamp in a string
+		string m_stime;
+
+		// the time stamp
 		std::chrono::steady_clock::time_point m_time;
+
+		// reserve memory for the length of m_message
+		const std::size_t m_message_length_reserved;
 	};
 
 	class base_logger {
 	public:
-		base_logger(std::size_t log_count);
-
+		base_logger(std::size_t log_count, std::size_t message_length);
+		~base_logger();
 	protected:
-		const std::size_t m_log_count = 0;
+		const std::size_t m_message_length;
+		const std::size_t m_log_count;
 		std::vector<log*> m_logs_v;
 	};
 }
