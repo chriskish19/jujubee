b2f1719cb0f96474cbd4ffe9dab9917a00871934
chriskish19 <68289629+chriskish19@users.noreply.github.com>
2025-03-15 20:13:24 -0600

    working on logger

--- projects/main_project_jujubee/classes/logging/logs.cpp
+++ projects/main_project_jujubee/classes/logging/logs.cpp
@@ -1,3 +1,30 @@
+#include "logs.hpp"
 #include NAMES_INCLUDE
 #include LOGS_INCLUDE_PATH
 
+void classes::log::set_message(const string& message)
+{
+    m_message = message;
+}
+
+std::chrono::steady_clock::time_point classes::log::get_time()
+{
+    m_time = std::chrono::high_resolution_clock::now();
+    return m_time;
+}
+
+void classes::log::set_time()
+{
+    m_time = std::chrono::high_resolution_clock::now();
+}
+
+classes::base_logger::base_logger(std::size_t log_count)
+    :m_log_count(log_count)
+{
+    m_logs_v.reserve(m_log_count);
+
+    for (std::size_t i = 0; i < m_log_count; ++i) {
+        log* lp = new log;
+        m_logs_v.push_back(lp);
+    }
+}
--- projects/main_project_jujubee/classes/logging/logs.hpp
+++ projects/main_project_jujubee/classes/logging/logs.hpp
@@ -9,6 +9,32 @@
 
 #pragma once
 #include NAMES_INCLUDE
+#include DEPENDENCIES_INCLUDE_PATH
+#include CODES_INCLUDE_PATH
+
+namespace classes {
+	class log {
+	public:
+		log() = default;
+
+		void set_message(const string& message);
+		string get_message() { return m_message; }
+		std::chrono::steady_clock::time_point get_time();
+		void set_time();
+	protected:
+		string m_message;
+		std::chrono::steady_clock::time_point m_time;
+	};
+
+	class base_logger {
+	public:
+		base_logger(std::size_t log_count);
+
+	protected:
+		const std::size_t m_log_count = 0;
+		std::vector<log*> m_logs_v;
+	};
+}
 
 
 
